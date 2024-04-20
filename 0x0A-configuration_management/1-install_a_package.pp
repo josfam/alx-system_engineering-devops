fa5f98376cd83279da0a6cabed43eb79cf8732a9
josfam <josephamukun@tutanota.com>
2024-04-20 02:24:54 +0000

    Fix: Require pip3 and Werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,19 @@
 # installs flask version 2.1.0 from pip3
 
+package { 'pip3':
+  provider  => 'pip3'
+}
+
+package { 'Werkzeug':
+  ensure => '2.1.1'
+}
+
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['pip3']
+  require  => [
+    Package['pip3'],
+    Package['Werkzeug']
+  ]
 }
 
