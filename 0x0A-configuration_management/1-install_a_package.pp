70e7305fac05d1541a3c601c6eab011386cc9ffd
josfam <josephamukun@tutanota.com>
2024-04-20 01:45:04 +0000

    Simplify to just flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,7 @@
 # installs flask version 2.1.0 from pip3
 
-package {'Python':
-  ensure => '3.8.10'
-}
-
-package {'Werkzeug':
-  ensure => '2.1.1'
-}
-
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
 
