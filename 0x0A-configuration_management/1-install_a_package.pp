637a2f228c500d93a51c23ac13d3717ac6ba94a1
josfam <josephamukun@tutanota.com>
2024-04-22 10:54:43 +0000

    Fix: Install python3 & Werkzeug first

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,16 @@
 # installs flask version 2.1.0 from pip3
 
-package { 'pip3':
-  provider  => 'pip3'
+package {'python3':
+  ensure => 'present',
 }
 
-package { 'Werkzeug':
-  ensure => '2.1.1'
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
 
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => [
-    Package['pip3'],
-    Package['Werkzeug']
-  ]
 }
 
