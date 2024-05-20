7a560034451a4c0cdd30d7582c843a5a9e18d093
josfam <jamukun@proton.me>
2024-05-20 17:58:41 +0300

    Use `get` for dict access, execute if ran directly

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,32 +2,34 @@
 
 """Gathers data from an api"""
 
-import requests
-import sys
+if __name__ == '__main__':
+    import requests
+    import sys
 
-employee_id = sys.argv[1]
+    employee_id = sys.argv[1]
 
-user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-todos_url = 'https://jsonplaceholder.typicode.com/todos'
-user = requests.get(user_url).json()
-todos = requests.get(todos_url).json()
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+        employee_id)
+    todos_url = 'https://jsonplaceholder.typicode.com/todos'
+    user = requests.get(user_url).json()
+    todos = requests.get(todos_url).json()
 
-completed_task_count = 0
-total_tasks = 0
-task_names = []
-name = user['name']
+    completed_task_count = 0
+    total_tasks = 0
+    task_names = []
+    name = user.get('name')
 
-# extract information for the user with a matching employee id
-for todo in todos:
-    if not todo['userId'] == int(employee_id):
-        continue
-    total_tasks += 1
-    if todo['completed']:
-        completed_task_count += 1
-    task_names.append(todo['title'])
+    # extract information for the user with a matching employee id
+    for todo in todos:
+        if not todo.get('userId') == int(employee_id):
+            continue
+        total_tasks += 1
+        if todo.get('completed'):
+            completed_task_count += 1
+        task_names.append(todo.get('title'))
 
-print('Employee {} is done with tasks({}/{})'.format(
-    name, completed_task_count, total_tasks))
+    print('Employee {} is done with tasks({}/{})'.format(
+        name, completed_task_count, total_tasks))
 
-for task_name in task_names:
-    print('\t {}'.format(task_name))
+    for task_name in task_names:
+        print('\t {}'.format(task_name))
