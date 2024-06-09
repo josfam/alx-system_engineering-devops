8e633de1c45423b80548bde3208231ce0db98f62
josfam <jamukun@proton.me>
2024-06-09 14:25:36 +0300

    Fix: Remove commandline argument from function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,6 @@ def number_of_subscribers(subreddit):
     import requests
     import sys
 
-    subreddit = sys.argv[1]
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     custom_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML,\
          like Gecko) Chrome/125.0.0.0 Safari/537.36'
